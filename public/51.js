6dc8f50456a7f59c601fcf4032d3150ccc2acf0e
trong nguyen <nguyentranhiep96@gmail.com>
2020-08-03 18:48:59 +0700

    first commit

---
+++ public/51.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[51],{
+
+/***/ "./resources/js/lang/en.json":
+/*!***********************************!*\
+  !*** ./resources/js/lang/en.json ***!
+  \***********************************/
+/*! exports provided: welcomeMessage, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"welcomeMessage\":\"Hello\"}");
+
+/***/ })
+
+}]);
